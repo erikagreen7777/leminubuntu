d550132cc19f5747074a8efa7ca7665286dd7347
erikagreen7777 <erikagreen7777@gmail.com>
2018-02-10 15:50:36 -0800

    preliminary clearning

--- lemin.h
+++ lemin.h
@@ -59,7 +59,7 @@ t_node 	*createNode(int v);
 t_graph *createGraph(t_info *data);
 void 	addEdge(t_info *data, t_graph *graph, int src, int dest);
 void    addEdgeString(t_graph *graph, char *src, char *dest);
-void 	printGraph(t_graph *graph,t_info *data);
+void 	printGraph(t_info *data);
 int 	DFS(t_graph *graph, t_info *data, int targetindex);
 void    find_start(t_info *data);
 void    validate(t_info *data);
